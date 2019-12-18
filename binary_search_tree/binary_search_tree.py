4a540b4d95c07f558ed24b5d6c61446fca8aab48
bquizza5 <blaine555@gmail.com>
2019-12-18 12:25:14 -0700

    binary search tree day 1

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+# from dll_queue import Queue
+# from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -12,21 +12,52 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        else:
+            if target > self.value:
+                if self.right == None:
+                    return False
+                else:
+                    return self.right.contains(target)
+            elif target < self.value:
+                if self.left == None:
+                    return False
+                else:
+                    return self.left.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        newMax = self
+        while newMax.right != None:
+            newMax = newMax.right
+        return newMax.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        
+        if self.left != None:
+            self.left.for_each(cb)
+        if self.right != None:
+            self.right.for_each(cb)
+        return cb(self.value)
+
 
     # DAY 2 Project -----------------------
 
